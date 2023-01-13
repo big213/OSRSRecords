819291468b70ac4e5131c0e4d78a66e75945c4dc
James Chang <jc4095@nyu.edu>
2023-01-12 23:12:58 -0500

    Implement retry logic on handleDiscordInteraction function

--- backend/functions/src/misc/handleDiscordInteraction/services/api.ts
+++ backend/functions/src/misc/handleDiscordInteraction/services/api.ts
@@ -5,14 +5,26 @@ const prodResource = axios.create({
   baseURL: env.site.api_url,
 });
 
-export async function executeGiraffeql(query) {
+export async function executeGiraffeql(query, attempts = 0) {
   const request = {
     headers: {
       "x-api-key": env.site.api_key,
     },
   };
 
-  const { data } = await prodResource.post("/giraffeql", query, request);
+  try {
+    const { data } = await prodResource.post("/giraffeql", query, request);
 
-  return data.data;
+    return data.data;
+  } catch(err: any) {
+    // check response type. if it is not an object, must be due to some general function failure. re-attempt up to 3 times before throwing the err
+    if (attempts < 3 && typeof err.response.data !== "object") {
+      return executeGiraffeql({
+        query,
+        attempts: attempts + 1,
+      });
+    }
+    throw err;
+  }
+
 }
